1293c54dc3e5ef93e7a4733056b4367f259e5f12
NaveenKummar <r.naveenkummar@gmail.com>
2024-10-06 19:23:18 +0530

    18_Mocling and Spying the functions_2

--- lambdas/putHandler.ts
+++ lambdas/putHandler.ts
@@ -1,10 +1,22 @@
 import {APIGatewayProxyHandler} from 'aws-lambda';
+import {DB} from '../lib/db';
 
-
+const db = new DB();
 
 export const handler: APIGatewayProxyHandler = async (e) => {
+    const body = JSON.parse(e.body || "{}");
 
-    return {body: "Something went wrong",
-        statusCode: 400,
+    if(!body.userId || !body.title){
+        return {
+            body: "Something went wrong",
+            statusCode: 400,
+        };
+    }
+
+    db.save();
+
+    return {
+        body: "Success!!",
+        statusCode: 200,
     };
 };
