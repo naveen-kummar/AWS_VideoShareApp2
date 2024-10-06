f98fa5fec6cf5433b45abc8d72eab15d90347d74
NaveenKummar <r.naveenkummar@gmail.com>
2024-10-06 18:56:18 +0530

    18_Mocling and Spying the functions_1

--- lambdas/__test__/putHandler.test.ts
+++ lambdas/__test__/putHandler.test.ts
@@ -7,15 +7,39 @@
  *      title <string>
  *      description? <string>
  *      tags? <string[]>
+ * If a valid body is passed, save the data in the database 
+ * Create pre-signed url
+ * Send that url to the client
  */
 
 import { handler } from "../putHandler";
+import {DB} from "../../lib/db";
 
 describe("Test for the Video Put Handler", () => {
 
+    afterEach(() => {
+        //restore the spy created with SpyOn
+        jest.resetAllMocks();
+    });
     test('Should return a 400 statuscode if emp', async () => {
         const res = await (handler as any)({body : JSON.stringify({})});
 
         expect(res.statusCode).toBe(400);
-    })
+    });
+
+    test('Should call db Save function if proper body is passed', async () => {
+        const spySave = jest.spyOn(DB.prototype, "save");
+
+        spySave.mockImplementation(async () => {});
+
+        const res = await (handler as any)({
+            body: JSON.stringify({
+                userId: "user-123",
+                title: "Cat-video",
+            }),
+        });
+
+        expect(spySave).toBeCalled();
+
+    });
 });
--- lambdas/putHandler.ts
+++ lambdas/putHandler.ts
@@ -1,6 +1,7 @@
 import {APIGatewayProxyHandler} from 'aws-lambda';
 
 
+
 export const handler: APIGatewayProxyHandler = async (e) => {
 
     return {body: "Something went wrong",
