2ddc51f65fd6a9d9f267df8ded2ab0b11f8d80f0
NaveenKummar <r.naveenkummar@gmail.com>
2024-12-07 20:04:24 +0530

    36_Creating  Lambda and S3 via cdk

---
+++ cf/firsk-cdk-app/lib/firsk-cdk-app-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda'
+import * as s3 from 'aws-cdk-lib/aws-s3'
+import { Construct } from 'constructs';
+
+
+export class FirskCdkAppStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    new lambda.Function(this, "my-lambda", {
+      code: lambda.Code.fromInline(
+        "exports.handler = async ()=> {console.log('hello')}"
+      ),
+      handler : "index.handler",
+      runtime : lambda.Runtime.NODEJS_18_X
+    });
+
+    new s3.CfnBucket(this, "my-bucket");
+  }
+}
---
+++ cf/firsk-cdk-app/test/firsk-cdk-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FirskCdkApp from '../lib/firsk-cdk-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/firsk-cdk-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FirskCdkApp.FirskCdkAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
