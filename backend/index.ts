68c0bc658782a4f02f327b8aea7cba409dfd15f3
NaveenKummar <r.naveenkummar@gmail.com>
2024-12-14 19:14:28 +0530

    39_Used nodejs to create entry function putHandler

--- backend/index.ts
+++ backend/index.ts
@@ -4,7 +4,7 @@ import { VidShareAppStack } from './stacks/vid-sahre';
 
 
 const app = new cdk.App();
-new VidShareAppStack(app, 'FirskCdkAppStack', {
+new VidShareAppStack(app, 'VidShareAppStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
--- backend/stacks/vid-sahre.ts
+++ backend/stacks/vid-sahre.ts
@@ -1,23 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda'
-import * as s3 from 'aws-cdk-lib/aws-s3'
+import * as lambdaFn from 'aws-cdk-lib/aws-lambda-nodejs'
 import { Construct } from 'constructs';
+import {resolve} from 'path'
 
 
 export class VidShareAppStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Put Handler
+    //new lambdaFn.NodejsFunction(this, "PutHandler", {
+   //   entry: resolve(__dirname, "../../lambdas/putHandler.ts")
+    //});
 
-    new lambda.Function(this, "my-lambda", {
-      code: lambda.Code.fromInline(
-        "exports.handler = async ()=> {console.log('hello')}"
-      ),
-      handler : "index.handler",
-      runtime : lambda.Runtime.NODEJS_18_X
-    });
-
-    new s3.CfnBucket(this, "my-bucket");
+  
   }
 }
