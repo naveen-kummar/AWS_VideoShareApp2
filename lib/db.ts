19edbe7238495922a767f5848add22d8843ecf3d
NaveenKummar <r.naveenkummar@gmail.com>
2024-12-25 18:58:49 +0530

    50_Used UpdateExpression command to update DynamoDB table

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,6 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { PutCommand, DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+import { PutCommand, DynamoDBDocumentClient, UpdateCommand } from "@aws-sdk/lib-dynamodb";
+import { Key } from "aws-cdk-lib/aws-kms";
 
 
 
@@ -34,5 +35,29 @@ export class DB<T extends { id: string }>{
 
     async update({id, attrs} : {id : string, attrs : Partial<Omit<T, 'id'>>}){
         console.log("Inside VideoDB update");
+        const UpdateExpressionArr: string[] = []; //set #title = :title, #description = :description
+        const ExpressionAttributeNames: Record<string, any> = {};
+        const ExpressionAttributeValues: Record<string, any> = {};
+
+        (Object.keys(attrs) as Array<keyof typeof attrs>).forEach((key) => {
+            ExpressionAttributeNames['#${String(key)}'] = key;
+            /*Below line will emit compile warning if you do not use 
+            "as Array<keyof typeof attrs>" in above line after "Object.keys(attrs)"
+            as key will be of type 'any' instead of string*/
+            ExpressionAttributeValues[':${String(key)}'] = attrs[key]; 
+            UpdateExpressionArr.push('#${String(key)} = :${String(key)}');
+        });
+
+ 
+
+        return new UpdateCommand({
+            TableName : this.config.tableName,
+            Key : {
+                id: id, //Input Param
+            },
+            UpdateExpression: 'set $(UpdateExpressionArr.join("," )',
+            ExpressionAttributeNames: {},
+            ExpressionAttributeValues: {},
+        })
     }
 }
