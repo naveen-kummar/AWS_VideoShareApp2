60472b851780660d9260f488673e4b58931cc2cf
NaveenKummar <r.naveenkummar@gmail.com>
2024-12-30 22:03:10 +0530

    58_Just decided whcih Video resolutions to support

--- lib/video-metadata.ts
+++ lib/video-metadata.ts
@@ -1,3 +1,16 @@
-export class VideoMetadata{
-    async frommUrl(url : string) {}
+interface Metadata{
+    width : number,
+    height : number,
+    fileSize : number
 }
+
+export class VideoMetadata{
+    async frommUrl(url : string) : Promise<Metadata>{
+        return undefined as any
+    }
+}
+
+// width >= 1280 --> 1280x720 & 640x360
+// 1280 < width <= 640 --> 640x360
+// 640 < width --> width
+
